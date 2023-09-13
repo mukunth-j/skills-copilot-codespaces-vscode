80b112bde79a671f8413c1961bb952cf89b934ac
Mukunth <jmukunth55@gmail.com>
2023-09-13 11:08:32 +0530

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember(){
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["Javascript", "HTML", "CSS"],
+        address: {
+            street: "123 Main St",
+            city: "New York",
+            state: "NY"
+        }
+    };
+    return member;
+}
